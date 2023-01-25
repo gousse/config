b7869d9dc1e1586d152c4430a360747aee60e029
Bruno Munoz <bruno@bmunoz.net>
2023-01-25 15:02:02 +0100

    update install mkcert to use latest

--- mkcert/install.sh
+++ mkcert/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-sudo apt install -y libnss3-tools
-
-curl -fsSL https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64 -o mkcert
-
-sudo mv mkcert /usr/local/bin/mkcert
-sudo chmod 755 /usr/local/bin/mkcert
-
+#sudo apt install -y libnss3-tools
+curl -JL "https://dl.filippo.io/mkcert/latest?for=linux/amd64" -o mkcert
+sudo install -o root -g root -m 0755 mkcert /usr/local/bin/mkcert
 mkcert -install
